6c27baac1b8b6ba5bf6d9a880d9a9a2b60e8f5cb
김예안 <kjan0929@naver.com>
2025-12-30 23:42:15 +0900

    feat(app): app 전체 실행 흐름 구현

--- 8th-racingcar/01/src/App.js
+++ 8th-racingcar/01/src/App.js
@@ -1,11 +1,14 @@
-import { Console } from '@woowacourse/mission-utils';
 import Input from './input.js';
+import PlayAllRound from './PlayGame.js';
+import Winner from './Winner.js';
 
 class App {
   async run() {
     const { parsedCarNames, parsedAttempts } = await Input.getInput();
-    Console.print(parsedCarNames);
-    Console.print(parsedAttempts);
+
+    const result = PlayAllRound.run(parsedCarNames, parsedAttempts);
+
+    Winner.announce(result);
   }
 }
 
--- 8th-racingcar/01/src/PlayGame.js
+++ 8th-racingcar/01/src/PlayGame.js
@@ -35,5 +35,7 @@ export default class PlayAllRound {
 
       Console.print('\n');
     });
+
+    return PlayAllRound.cars;
   }
 }
