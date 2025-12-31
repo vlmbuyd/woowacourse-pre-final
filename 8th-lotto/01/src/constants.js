fae6ef219af274fb9a8df89d332f75d769b747cd
김예안 <kjan0929@naver.com>
2025-12-31 16:50:48 +0900

    feat(constant): 메시지 및 매직 넘버 상수화

---
+++ 8th-lotto/01/src/constants.js
@@ -0,0 +1,23 @@
+export const INPUT_MSG = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '당첨 번호를 입력해 주세요.\n',
+});
+
+export const OUTPUT_MSG = Object.freeze({
+  PURCHASE_AMOUNT: (amount) => `\n${amount}개를 구매했습니다.`,
+  WINNING_STATS: '당첨 통계\n---\n',
+  PROFIT_RATE: (rate) => `총 수익률은 ${rate}%입니다.`,
+});
+
+export const ERROR_MSG = Object.freeze({
+  ERROR_PREFIX: '[ERROR] ',
+  INVALID_UNIT: '구입 금액은 1,000원 단위여야 합니다.',
+  OUT_OF_RANGE: '로또 번호는 6개여야 합니다.',
+});
+
+export const GAME_RULE = Object.freeze({
+  PRICE_UNIT: 1000,
+  LOTTO_NUM_AMOUNT: 6,
+  BONUS_NUM_AMOUNT: 1,
+});
