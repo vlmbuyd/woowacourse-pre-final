6ccb31740b2bf8d4c7a5063f94fb2c9962e66124
김예안 <kjan0929@naver.com>
2025-12-31 17:57:54 +0900

    feat(validator): 당첨 번호, 보너스 번호 입력 및 검증 로직 구현

---
+++ 8th-lotto/01/src/utils/Validator.js
@@ -0,0 +1,57 @@
+import { ERROR_MSG, GAME_RULE } from '../constants.js';
+
+export default class Validator {
+  static validatePurchaseAmount(purchaseAmount) {
+    if (Number(purchaseAmount) % GAME_RULE.PRICE_UNIT !== 0) {
+      throw new Error(`${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.INVALID_UNIT}`);
+    }
+  }
+
+  static validateWinningNums(winningNum) {
+    const winningNumList = winningNum.split(',');
+
+    // 당첨 번호 6개인지
+    if (winningNumList.length !== GAME_RULE.LOTTO_NUM_AMOUNT)
+      throw new Error(
+        `${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.INVALID_WINNING_AMOUNT}`
+      );
+
+    // 중복된 숫자 없는지
+    const set = new Set(winningNumList);
+    const filteredArr = [...set];
+    if (filteredArr.length !== GAME_RULE.LOTTO_NUM_AMOUNT)
+      throw new Error(`${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.DUPLICATE_NUM}`);
+
+    // 1~45 사이의 숫자인지
+    if (
+      !winningNumList.every(
+        (num) =>
+          num >= GAME_RULE.LOTTO_MIN_NUM && num <= GAME_RULE.LOTTO_MAX_NUM
+      )
+    ) {
+      throw new Error(`${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.OUT_OF_RNAGE}`);
+    }
+  }
+
+  static validateBonusNum(bonusNum, winningNum) {
+    const set = new Set([...winningNum, ...bonusNum]);
+    const filteredArr = [...set];
+
+    // 당첨 번호 중 중복된 숫자가 있을 때
+    if (
+      filteredArr.length !==
+      GAME_RULE.LOTTO_NUM_AMOUNT + GAME_RULE.BONUS_NUM_AMOUNT
+    )
+      throw new Error(`${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.DUPLICATE_NUM}`);
+
+    // 1~45 사이의 숫자인지
+    if (
+      !(
+        bonusNum >= GAME_RULE.LOTTO_MIN_NUM &&
+        bonusNum <= GAME_RULE.LOTTO_MAX_NUM
+      )
+    ) {
+      throw new Error(`${ERROR_MSG.ERROR_PREFIX}${ERROR_MSG.OUT_OF_RNAGE}`);
+    }
+  }
+}
--- 8th-lotto/01/src/view/InputView.js
+++ 8th-lotto/01/src/view/InputView.js
@@ -16,4 +16,28 @@ export default class InputView {
       InputView.readPurchaseAmount();
     }
   }
+
+  static async readWinningNums() {
+    try {
+      const winningNum = await Console.readLineAsync(INPUT_MSG.WINNING_NUMBER);
+      Validator.validateWinningNums(winningNum);
+
+      return winningNum.split(',');
+    } catch (err) {
+      Console.print(err);
+      InputView.readWinningNums();
+    }
+  }
+
+  static async readBonusNum(winningNum) {
+    try {
+      const bonusNum = await Console.readLineAsync(INPUT_MSG.BONUS_NUMBER);
+      Validator.validateBonusNum(bonusNum, winningNum);
+
+      return Number(bonusNum);
+    } catch (err) {
+      Console.print(err);
+      InputView.readBonusNum(winningNum);
+    }
+  }
 }
