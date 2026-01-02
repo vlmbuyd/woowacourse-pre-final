fef653632102a64ebd570e8a8b8f9b2c505322fc
김예안 <kjan0929@naver.com>
2026-01-02 23:18:06 +0900

    feat(validator): 출석 확인 예외 검증 로직 추가

--- 7th-attendance/01/src/utils/Validator.js
+++ 7th-attendance/01/src/utils/Validator.js
@@ -1,19 +1,27 @@
-import { ERROR_MSG, VALIDATION_KEY } from '../constants.js';
-
-const validators = {
-  [VALIDATION_KEY.SELECTION_PROMPT]: (value) =>
-    Validator.validateSelectionPrompt(value),
-};
+import { ERROR_MSG } from '../constants.js';
 
 export default class Validator {
-  static validate(key, inputValue) {
-    const validator = validators[key];
-    if (validator) validator(inputValue);
-  }
-
-  static validateSelectionPrompt(inputPrompt) {
-    if (!['1', '2', '3', '4', 'Q'].includes(inputPrompt)) {
+  static validateSelectionPrompt(mode) {
+    if (!['1', '2', '3', '4', 'Q'].includes(mode)) {
       throw new Error(`${ERROR_MSG.PREFIX} 잘못된 값을 입력하였습니다.`);
     }
+  }
+
+  // 등록되지 않은 닉네임
+  static validateInvalidNickname(nickname, attendanceRecords) {
+    if (!attendanceRecords.some((record) => record[0] === nickname)) {
+      throw new Error(`${ERROR_MSG.PREFIX} 등록되지 않은 닉네임입니다.`);
+    }
+  }
+
+  // 시간 형식
+  static validateAttendanceCheck(time) {
+    const [hour, min] = time.split(':');
+
+    if (
+      !(Number(hour) >= 0 && Number(hour) <= 24) ||
+      !(Number(min) >= 0 && Number(min) <= 60)
+    )
+      throw new Error(`${ERROR_MSG.PREFIX} 잘못된 형식을 입력하였습니다.`);
   }
 }
--- 7th-attendance/01/src/view/InputView.js
+++ 7th-attendance/01/src/view/InputView.js
@@ -1,12 +1,38 @@
 import { Console } from '@woowacourse/mission-utils';
-import { INPUT_MSG, VALIDATION_KEY } from '../constants.js';
+import { ATTENDANCE_MODE, INPUT_MSG, VALIDATION_KEY } from '../constants.js';
 import Validator from '../utils/Validator.js';
 
+const inputs = {
+  [ATTENDANCE_MODE['1']]: (...rest) => InputView.checkAttendance(...rest),
+  [ATTENDANCE_MODE['2']]: (...rest) => InputView.editAttendance(...rest),
+  [ATTENDANCE_MODE['3']]: (...rest) =>
+    InputView.checkCrewAttendanceHistory(...rest),
+};
+
 export default class InputView {
+  static async readInput(key, ...rest) {
+    const input = inputs[key];
+    if (input) return await input(...rest);
+  }
+
   static async selectFunc() {
     const mode = await Console.readLineAsync(INPUT_MSG.SELECT);
-    Validator.validate(VALIDATION_KEY.SELECTION_PROMPT, mode);
+    Validator.validateSelectionPrompt(mode);
 
     return mode;
   }
+
+  static async checkAttendance(attendanceRecords) {
+    const nickname = await Console.readLineAsync('\n닉네임을 입력해 주세요.\n');
+    Validator.validateInvalidNickname(nickname, attendanceRecords);
+
+    const time = await Console.readLineAsync('\n등교 시간을 입력해 주세요.\n');
+    Validator.validateAttendanceCheck(time);
+
+    return { nickname, time };
+  }
+
+  static async editAttendance() {}
+
+  static async checkCrewAttendanceHistory() {}
 }
