7e093dcd16ae3ebd72254ab0e3bd194ffbb11265
김예안 <kjan0929@naver.com>
2026-01-02 23:28:55 +0900

    refactor(parser): 출석 상태 가공 로직 개선

--- 7th-attendance/01/src/utils/Parser.js
+++ 7th-attendance/01/src/utils/Parser.js
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import path from 'path';
 import AttendanceManager from '../model/AttendanceManager.js';
+import { ATTENDANCE_STATUS, RULE } from '../constants.js';
 
 export class CSVParser {
   static parseToArr(csvPath) {
@@ -32,16 +33,50 @@ export class CSVParser {
 }
 
 export class DataParser {
+  constructor(attendanceRecords) {
+    this.attendanceRecords = attendanceRecords;
+  }
+
+  // 주어진 날짜를 바탕으로 출석/지각/결석 값 반환
+  getAttendanceStatus(date) {
+    const day = new Date(date);
+    const dayOfWeek = day.getDay(); // 요일
+    const timeStamp = date.slice(-5); // 시간 e.g. 13:02
+    const [hour, min] = timeStamp.split(':');
+
+    if (dayOfWeek === 1) {
+      /**
+       * 월요일
+       */
+      // 결석
+      if (Number(hour) > 13 || Number(min) > RULE.ABSENCE_MIN)
+        return ATTENDANCE_STATUS.ABSENCE;
+      // 지각
+      else if (Number(hour) === 13 && Number(min) > RULE.LATE_MIN)
+        return ATTENDANCE_STATUS.LATE;
+
+      return ATTENDANCE_STATUS.ATTENDANCE;
+    } else if ([2, 3, 4, 5].includes(dayOfWeek)) {
+      /**
+       * 화-금요일
+       */
+      if (Number(hour) > 10 || Number(min) > RULE.ABSENCE_MIN)
+        return ATTENDANCE_STATUS.ABSENCE; // 결석
+      else if (Number(hour) === 10 && Number(min) > RULE.LATE_MIN)
+        return ATTENDANCE_STATUS.LATE; // 지각
+
+      return ATTENDANCE_STATUS.ATTENDANCE;
+    }
+  }
+
   // 기존의 출석 기록의 출석 상태 파악하고 플래그 추가
   // e.g. [ '짱수', '2024-12-03 10:00', '(출석)' ]
-  static determineAttendanceStatus(records) {
+  determineAttendanceStatus() {
     // TODO 출석 기록이 없는 경우 결석으로 간주
-    records.forEach((record) => {
+    this.attendanceRecords.forEach((record) => {
       const date = record[1];
-      const attendanceStatus = AttendanceManager.getAttendanceStatus(date);
+      const attendanceStatus = this.getAttendanceStatus(date);
       record.push(`(${attendanceStatus})`);
     });
-
-    console.log(records);
   }
 }
